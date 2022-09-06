40998e0aa97045847624f284ab3b22bce33fcf40
Patrick Santino <pet.santino@gmail.com>
2022-09-06 21:30:24 +0700

    Footer awal

--- resources/views/layouts/customer/app.blade.php
+++ resources/views/layouts/customer/app.blade.php
@@ -61,8 +61,7 @@
             <div class="xl:grid xl:grid-cols-3 xl:gap-8">
                 <div class="space-y-8 xl:col-span-1">
                     <img class="h-10" src={{ asset('images/logo.png') }} alt="Company name">
-                    <p class="text-base text-gray-500">Making the world a better place through constructing elegant
-                        hierarchies.</p>
+                    <p class="text-base text-gray-500">Cetak Kaos Satuan dan Lusinan. Kaos, Hoodie, Totebag, Bordir, Jersey dll.</p>
                     <div class="flex space-x-4">
                         <a href="#" class="text-gray-400 hover:text-gray-500">
                             <span class="sr-only">Instagram</span>
@@ -76,94 +75,6 @@
                     </div>
                 </div>
                 <div class="mt-12 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-                    <div class="md:grid md:grid-cols-2 md:gap-8">
-                        <div>
-                            <h3 class="text-base font-medium text-gray-900">Solutions</h3>
-                            <ul role="list" class="mt-4 space-y-4">
-                                <li>
-                                    <a href="#"
-                                        class="text-base text-gray-500 hover:text-gray-900">Marketing</a>
-                                </li>
-
-                                <li>
-                                    <a href="#"
-                                        class="text-base text-gray-500 hover:text-gray-900">Analytics</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Commerce</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Insights</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="mt-12 md:mt-0">
-                            <h3 class="text-base font-medium text-gray-900">Support</h3>
-                            <ul role="list" class="mt-4 space-y-4">
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Pricing</a>
-                                </li>
-
-                                <li>
-                                    <a href="#"
-                                        class="text-base text-gray-500 hover:text-gray-900">Documentation</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Guides</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">API
-                                        Status</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="md:grid md:grid-cols-2 md:gap-8">
-                        <div>
-                            <h3 class="text-base font-medium text-gray-900">Company</h3>
-                            <ul role="list" class="mt-4 space-y-4">
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">About</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Blog</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Jobs</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Press</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Partners</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="mt-12 md:mt-0">
-                            <h3 class="text-base font-medium text-gray-900">Legal</h3>
-                            <ul role="list" class="mt-4 space-y-4">
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Claim</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Privacy</a>
-                                </li>
-
-                                <li>
-                                    <a href="#" class="text-base text-gray-500 hover:text-gray-900">Terms</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
